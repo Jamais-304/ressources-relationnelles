b3733b23cee8e56d659666cd2ccc5d36bc18eba9
Quentin <quentindumon352a@gmail.com>
2025-02-22 17:50:27 +0100

    test(T:Server): add tests for the normalize method
    Refs: #23

---
+++ backend/tests/utils/normalize.test.ts
@@ -0,0 +1,13 @@
+import { normalize } from "../../utils/normailze.ts"
+
+describe('normalize', () => {
+    it('should return the correct port number when the input is a valid numeric string', () =>
+        expect(normalize('4550')).toBe(4550)
+    )
+    it('should return the original string if the input is not a valid number', () =>
+        expect(normalize('abc')).toBe('abc')
+    )
+    it('should throw an error if the input is <= 0', () => {
+        expect(() => normalize('-1')).toThrowError('Value cannot be ≤ to 0: -1')
+    })
+})
