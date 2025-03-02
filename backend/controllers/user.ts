d102c710045f17f35c556bf5c225c5b08159b23d
Quentin <quentindumon352a@gmail.com>
2025-03-02 11:41:50 +0100

    style(C:Users): update indentation
    Refs: #8

--- backend/controllers/user.ts
+++ backend/controllers/user.ts
@@ -1,29 +1,44 @@
-import {Request, Response} from 'express'
+import type { Request, Response } from 'express'
 import User from '../models/User.ts'
-import bcrype from  'bcrypt'
+import bcrypt from 'bcrypt'
 
-export const createUser = async (req: Request, res: Response) => {
-    try{
+export const createUser = async (req: Request, res: Response)=> {
+    try {
 
-        const {email, password, pseudonyme, role} = req.body
+        const { email, password, pseudonyme, role } = req.body
         const saltRounds = 10
-        const hashedPassword = await bcrype.hash(password, saltRounds)
+        const hashedPassword = await bcrypt.hash(password, saltRounds)
         
+        const existingUser = await User.findOne({ email })
+        
+        if (existingUser) {
+            return res.status(400).json({ message: 'L\'email est déjà utilisé' })
+        }
+
         const newUser = new User({
-            email: email, 
-            password: hashedPassword, 
-            pseudonyme: pseudonyme, 
+            email: email,
+            password: hashedPassword,
+            pseudonyme: pseudonyme,
             role: role,
         })
-        
+
         await newUser.save()
 
-        return res.status(201).json({message: 'Utilisateur crée avec succès', user: newUser})
+        return res.status(201).json(
+            { 
+                message: 'Utilisateur crée avec succès',
+                 user: newUser 
+                }
+            )
 
-    } catch (error){
+    } catch (error) {
 
         console.error(error)
-        return res.status(500).json({message: 'Erreur serveur', error})
+        return res.status(500).json(
+            {
+                message: 'Erreur serveur',
+                error 
+            })
 
     }
 }
--- backend/models/User.ts
+++ backend/models/User.ts
@@ -1,4 +1,4 @@
-import mongoose, {Document, Schema} from 'mongoose'
+import mongoose, { Document, Schema } from 'mongoose'
 import uniqueValidator from 'mongoose-unique-validator'
 
 interface IUser extends Document {
@@ -12,39 +12,39 @@ const userSchema: Schema<IUser> = new mongoose.Schema(
     {
         email: {
             type: String,
-            required: [true,'L\'email est requis'],
+            required: [true, 'L\'email est requis'],
             unique: true,
             trim: true,
-            match: [/^\S+@\S+\.\S+$/, 'Adresse email invalide'] 
+            match: [/^\S+@\S+\.\S+$/, 'Adresse email invalide']
         },
         password: {
             type: String,
-            required: [true, 'Le mot de passe est requis'], 
-            trim: true, 
+            required: [true, 'Le mot de passe est requis'],
+            trim: true,
             minlength: [8, 'Le mot de passe doit contenir au moins 8 caractères'],
-            maxlength: [55, 'Le mot de passe ne doit pas dépasser 55 caractères'], 
+            maxlength: [55, 'Le mot de passe ne doit pas dépasser 55 caractères'],
             match: [
                 /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+={}\[\]:;"\'<>,.?/\\|`~-]).{6,55}$/,
                 'Le mot de passe doit contenir au moins une lettre, un chiffre et un caractère spécial.'
             ]
         },
         pseudonyme: {
-            type: String, 
-            required: [true, 'Le pseudonyme est requis'], 
-            trim: true, 
-            minlength: [5, 'Le pseudonyme doit contenir au moins 5 caractères'], 
+            type: String,
+            required: [true, 'Le pseudonyme est requis'],
+            trim: true,
+            minlength: [5, 'Le pseudonyme doit contenir au moins 5 caractères'],
             maxlength: [40, 'Le pseudonyme de passe ne doit pas dépasser 40 caractères']
         },
-        role: { 
-            type: String, 
-            required: [true, 'Le rôle est requis'], 
+        role: {
+            type: String,
+            required: [true, 'Le rôle est requis'],
             enum: {
                 values: ['super-administrateur', 'administrateur', 'moderateur', 'utilisateur'],
                 message: 'Le rôle "{VALUE}" est invalide.'
             },
             default: 'utilisateur'
         }
-    }, 
+    },
     {
         timestamps: true
     }
