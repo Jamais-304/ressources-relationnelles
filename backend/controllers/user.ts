2d13df3ce37b5ecd906c27d5228e29a09b2cf978
Quentin <quentindumon352a@gmail.com>
2025-03-02 10:47:24 +0100

    feat(C:Users): create controller user
    Refs: #8

---
+++ backend/controllers/user.ts
@@ -0,0 +1,29 @@
+import {Request, Response} from 'express'
+import User from '../models/User.ts'
+import bcrype from  'bcrypt'
+
+export const createUser = async (req: Request, res: Response) => {
+    try{
+
+        const {email, password, pseudonyme, role} = req.body
+        const saltRounds = 10
+        const hashedPassword = await bcrype.hash(password, saltRounds)
+        
+        const newUser = new User({
+            email: email, 
+            password: hashedPassword, 
+            pseudonyme: pseudonyme, 
+            role: role,
+        })
+        
+        await newUser.save()
+
+        return res.status(201).json({message: 'Utilisateur crée avec succès', user: newUser})
+
+    } catch (error){
+
+        console.error(error)
+        return res.status(500).json({message: 'Erreur serveur', error})
+
+    }
+}
