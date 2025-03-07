04e1c51396b2ce7a9f32e53b6cfb7e69aa3a504c
Quentin <quentindumon352a@gmail.com>
2025-03-07 16:59:10 +0100

    feat(C:Users): update controller User
    Refs: #8

--- backend/controllers/user.ts
+++ backend/controllers/user.ts
@@ -1,44 +1,59 @@
 import type { Request, Response } from 'express'
 import User from '../models/User.ts'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
-export const createUser = async (req: Request, res: Response)=> {
+export const createUser = async (req: Request, res: Response): Promise<Response> => {
     try {
-
-        const { email, password, pseudonyme, role } = req.body
-        const saltRounds = 10
-        const hashedPassword = await bcrypt.hash(password, saltRounds)
-        
-        const existingUser = await User.findOne({ email })
-        
-        if (existingUser) {
-            return res.status(400).json({ message: 'L\'email est déjà utilisé' })
-        }
-
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
         const newUser = new User({
-            email: email,
+            email: req.body.email,
             password: hashedPassword,
-            pseudonyme: pseudonyme,
-            role: role,
+            pseudonyme: req.body.pseudonyme,
+            role: req.body.role,
         })
 
         await newUser.save()
 
-        return res.status(201).json(
-            { 
-                message: 'Utilisateur crée avec succès',
-                 user: newUser 
-                }
-            )
+        return res.status(201).json({
+            message: 'Utilisateur crée avec succès',
+            user: newUser
+        })
 
-    } catch (error) {
-
+    } catch (error: unknown) {
         console.error(error)
-        return res.status(500).json(
-            {
-                message: 'Erreur serveur',
-                error 
-            })
+        return res.status(500).json({ message: 'Erreur serveur', error })
+    }
+}
 
+export const loginUser = async (req: Request, res: Response): Promise<Response> => {
+    try {
+
+        const user = await User.findOne({ email: req.body.email })
+        const secretKey: string | undefined = process.env.TOKEN_SECRET
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Paire identifiant/mot de passe incorrecte !",
+            })
+        }
+        const isValid = await bcrypt.compare(req.body.password, user.password)
+        if (!isValid) {
+            return res.status(401).json({
+                message: "Paire identifiant/mot de passe incorrecte !",
+            })
+        }
+        if (!secretKey) {
+            console.error('La clé secrète n\'est pas définie')
+            return res.status(500).json({ message: 'Erreur serveur : clé secrète manquante' })
+        }
+        const token = jwt.sign({ userId: user._id, role: user.role }, secretKey, { expiresIn: '1h' })
+
+        return res.status(200).json({ token })
+
+
+    } catch (error: unknown) {
+        console.error(error)
+        return res.status(500).json({ message: 'Erreur serveur', error })
     }
 }
