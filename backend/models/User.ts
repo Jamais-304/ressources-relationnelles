5b3a71ac3519bf494bcd56c4d800e7df0dead970
Quentin <quentindumon352a@gmail.com>
2025-03-08 15:20:09 +0100

    feat(M:User): update user model replace singles quotes with double quotes
    Refs: #8

--- backend/models/User.ts
+++ backend/models/User.ts
@@ -1,48 +1,45 @@
-import mongoose, { Document, Schema } from 'mongoose'
-import uniqueValidator from 'mongoose-unique-validator'
-
-interface IUser extends Document {
-    email: string;
-    password: string;
-    pseudonyme: string;
-    role: 'super-administrateur' | 'administrateur' | 'moderateur' | 'utilisateur'
+import mongoose, { Document, Schema } from "mongoose"
+import uniqueValidator from "mongoose-unique-validator"
+interface UserInterface {
+    email: string
+    password: string
+    pseudonyme: string
+    role: "super-administrateur" | "administrateur" | "moderateur" | "utilisateur"
 }
 
-const userSchema: Schema<IUser> = new mongoose.Schema(
+interface IUserSchema extends UserInterface, Document { }
+
+const userSchema: Schema<IUserSchema> = new mongoose.Schema(
     {
         email: {
             type: String,
-            required: [true, 'L\'email est requis'],
+            required: [true, "L'email est requis"],
             unique: true,
             trim: true,
-            match: [/^\S+@\S+\.\S+$/, 'Adresse email invalide']
+            match: [/^\S+@\S+\.\S+$/, "Adresse email invalide"]
         },
         password: {
             type: String,
-            required: [true, 'Le mot de passe est requis'],
+            required: [true, "Le mot de passe est requis"],
             trim: true,
-            minlength: [8, 'Le mot de passe doit contenir au moins 8 caractères'],
-            maxlength: [90, 'Le mot de passe ne doit pas dépasser 90 caractères'],
-            // match: [
-            //     /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+={}\[\]:;"\'<>,.?/\\|`~-]).{6,55}$/,
-            //     'Le mot de passe doit contenir au moins une lettre, un chiffre et un caractère spécial.'
-            // ]
+            minlength: [8, "Le mot de passe doit contenir au moins 8 caractères"],
+            maxlength: [90, "Le mot de passe ne doit pas dépasser 90 caractères"]
         },
         pseudonyme: {
             type: String,
-            required: [true, 'Le pseudonyme est requis'],
+            required: [true, "Le pseudonyme est requis"],
             trim: true,
-            minlength: [5, 'Le pseudonyme doit contenir au moins 5 caractères'],
-            maxlength: [40, 'Le pseudonyme de passe ne doit pas dépasser 40 caractères']
+            minlength: [5, "Le pseudonyme doit contenir au moins 5 caractères"],
+            maxlength: [40, "Le pseudonyme de passe ne doit pas dépasser 40 caractères"]
         },
         role: {
             type: String,
-            required: [true, 'Le rôle est requis'],
+            required: [true, "Le rôle est requis"],
             enum: {
-                values: ['super-administrateur', 'administrateur', 'moderateur', 'utilisateur'],
+                values: ["super-administrateur", "administrateur", "moderateur", "utilisateur"],
                 message: 'Le rôle "{VALUE}" est invalide.'
             },
-            default: 'utilisateur'
+            default: "utilisateur"
         }
     },
     {
@@ -52,6 +49,6 @@ const userSchema: Schema<IUser> = new mongoose.Schema(
 
 userSchema.plugin(uniqueValidator)
 
-const User = mongoose.model<IUser>('User', userSchema)
+const User = mongoose.model<IUserSchema>("User", userSchema)
 
 export default User
