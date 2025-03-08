771a4ec60c761be064a0e63bb574f6eeda613089
Quentin <quentindumon352a@gmail.com>
2025-03-08 15:15:14 +0100

    refactor(C:Users): add config Prettier and replace all singles quotes doubles quotes
    Refs: #8

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
-import { PORT_BACKEND } from './config.ts'
-import app from './src/app.ts'
-import http from 'http'
-import {normalize} from './utils/normailze.ts'
+import { PORT_BACKEND } from "./config.ts"
+import app from "./src/app.ts"
+import http from "http"
+import { normalize } from "./utils/normailze.ts"
 
 /**
  * Normalize the port value.
@@ -10,32 +10,32 @@ import {normalize} from './utils/normailze.ts'
  * - Throws an error if the value is <= 0.
  * - Returns the original string if it's not a number.
  * - Returns `false` in unexpected cases (fallback).
-*
-* @param {string} portBackend - The port value as a string.
-* @returns {string | number | false} - The normalized port.
-*/
+ *
+ * @param {string} portBackend - The port value as a string.
+ * @returns {string | number | false} - The normalized port.
+ */
 
-const port = normalize(PORT_BACKEND || '3000')
+const port = normalize(PORT_BACKEND || "3000")
 
-app.set('port', port) // Set port configuration in App
+app.set("port", port) // Set port configuration in App
 
 const errorHandler = (error: NodeJS.ErrnoException) => {
     // If the error is not related to the 'listen' syscall, throw it
-    if (error.syscall !== 'listen') {
-        throw error; // Error handling for higher-level issues
+    if (error.syscall !== "listen") {
+        throw error // Error handling for higher-level issues
     }
     // Retrieve the server's address and store it in the constant `address`
     const address = server.address()
     // Create an informative message for error handling, depending on the address type
-    const bind = typeof address === 'string' ? `pipe ${address}` : `port ${port}`
+    const bind = typeof address === "string" ? `pipe ${address}` : `port ${port}`
     // Handle specific error codes related to server binding
     switch (error.code) {
-        case 'EACCES':
+        case "EACCES":
             // Permission denied: the required privilege is missing
             console.error(`${bind} requires elevated privileges`)
             process.exit(1) // Exit the process with an error code (1), indicating failure
-            break;
-        case 'EADDRINUSE':
+            break
+        case "EADDRINUSE":
             // Address already in use: the port or pipe is occupied
             console.error(`${bind} is already in use`)
             process.exit(1)
@@ -46,32 +46,30 @@ const errorHandler = (error: NodeJS.ErrnoException) => {
     }
 }
 const server = http.createServer(app)
-server.on('error', errorHandler)
-server.on('listening', () => {
-    const portEmot: { [key: string]: string } = {  // Déclaration du type d'index
-        '0': '0️⃣',
-        '1': '1️⃣',
-        '2': '2️⃣',
-        '3': '3️⃣',
-        '4': '4️⃣',
-        '5': '5️⃣',
-        '6': '6️⃣',
-        '7': '7️⃣',
-        '8': '8️⃣',
-        '9': '9️⃣',
+server.on("error", errorHandler)
+server.on("listening", () => {
+    const portEmot: { [key: string]: string } = {
+        // Déclaration du type d'index
+        "0": "0️⃣",
+        "1": "1️⃣",
+        "2": "2️⃣",
+        "3": "3️⃣",
+        "4": "4️⃣",
+        "5": "5️⃣",
+        "6": "6️⃣",
+        "7": "7️⃣",
+        "8": "8️⃣",
+        "9": "9️⃣"
     }
     const portSplit = port
         .toString()
-        .split('')
+        .split("")
         .map((digit) => portEmot[digit])
-        .join('  ')
+        .join("  ")
     const address = server.address()
-    const bind = typeof address === 'string' ? `pipe ${address}` : `port ${portSplit}`
-    console.log('Server start 🛫')
+    const bind = typeof address === "string" ? `pipe ${address}` : `port ${portSplit}`
+    console.log("Server start 🛫")
     console.log(`Listening 👂 on ${bind}`)
 })
 
 server.listen(port)
-
-
-
