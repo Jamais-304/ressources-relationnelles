17616b7ce20e8d52fac5be6b543d5067eb119f6f
Quentin <quentindumon352a@gmail.com>
2025-03-08 15:23:34 +0100

    feat(C:Users): update userValidator and add refreshTokenValidation
    Refs: #8

--- backend/middleware/validators/userValidator.ts
+++ backend/middleware/validators/userValidator.ts
@@ -6,9 +6,9 @@ export const signUpUserValidationRules = [
     body("email")
         .isEmail()
         .withMessage("L'email doit être une adresse email valide")
-        .normalizeEmail()  // Normaliser l'email
+        .normalizeEmail() // Normaliser l'email
         .escape()
-        .trim(),  // Enlever les espaces en début et fin de chaîne
+        .trim(), // Enlever les espaces en début et fin de chaîne
     // Le mot de passe : chaîne de caractères , obligatoire, échapper les caractères spéciaux
     body("password")
         .isString()
@@ -22,7 +22,7 @@ export const signUpUserValidationRules = [
         .isLength({ min: 5, max: 40 })
         .withMessage("Le pseudonyme doit contenir au moins 5 caractère et faire moins de 40 caractères")
         .escape()
-        .trim(), // Enlever les espaces en début et fin de chaîne
+        .trim() // Enlever les espaces en début et fin de chaîne
 ]
 
 // Règles de validation pour la connexion de l'utilisateur
@@ -31,7 +31,7 @@ export const loginUserValidationRules = [
     body("email")
         .isEmail()
         .withMessage("L'email doit être une adresse email valide")
-        .normalizeEmail()  // Normaliser l'email
+        .normalizeEmail() // Normaliser l'email
         .escape()
         .trim(), // Enlever les espaces en début et fin de chaîne
     // Le mot de passe : chaîne de caractères , obligatoire, échapper les caractères spéciaux
@@ -40,5 +40,14 @@ export const loginUserValidationRules = [
         .isLength({ min: 8, max: 25 })
         .withMessage("Le mot de passe doit contenir au moins 8 caractère et faire moins de 26 caractères")
         .escape()
-        .trim(),
+        .trim()
 ]
+// Règles pour le refreshtoken
+export const refreshTokenValidationRule = [
+    body("refreshToken")
+        .isString()
+        .withMessage("Le refresh token doit être une chaîne de caractères")
+        .escape()
+        .trim() // Enlever les espaces en début et fin de chaîne
+]
+
