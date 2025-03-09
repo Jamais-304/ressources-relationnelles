6b66cbc4f33adeb11e7f4b3cb954e20fa3f0bfea
maelbrize <94190489+maelbrize@users.noreply.github.com>
2025-03-09 14:38:31 +0100

    feat(M:Resource): add author and validation fields to resource model

--- backend/models/Resource.ts
+++ backend/models/Resource.ts
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 
 const resourceSchema = new mongoose.Schema({
+	authorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 	title: { type: String, required: true, trim: true },
 	contentGridfsId: { type: String, required: true },
 	category: { type: String, enum:['TEXT','HTML','VIDEO','AUDIO','IMAGE'], required: true, trim: true },
 	relationType: { type: String, required: true, trim: true },
 	status: { type: String, enum: ['DRAFT', 'PENDING', 'PUBLISHED'], default: 'DRAFT' },
+	validatedAndPublishedAt: { type: Date },
+	validatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 
-}, { timestamps: true });
+}, { timestamps: true }); // createdAt, updatedAt
 
 export default mongoose.model('Resource', resourceSchema);
