4657a962ddc03bb820434fa37b8f8c5c28a34214
Quentin <quentindumon352a@gmail.com>
2025-03-09 17:26:54 +0100

    feat(C:Users): add route for admin create users
    Refs: #8

--- backend/router/userRoutes.ts
+++ backend/router/userRoutes.ts
@@ -1,16 +1,22 @@
 import { Router } from "express"
-import { createUser, loginUser, logoutUser, refreshToken } from "../controllers/userControllers.ts"
-import { loginUserValidationRules, signUpUserValidationRules, refreshTokenValidationRule } from "../middleware/validators/userValidator.ts"
+import { createUser, loginUser, logoutUser, refreshToken, adminCreateUser } from "../controllers/userControllers.ts"
 import { validationErrorHandler } from "../middleware/validationErrorHandler.ts"
+import { auth } from "../middleware/auth.ts"
+import { loginUserValidationRules, signUpUserValidationRules, refreshTokenValidationRule } from "../middleware/validators/userValidator.ts"
+
 const router = Router()
 
-// POST /api/users/signup
-router.post("/v1/users/signup", signUpUserValidationRules, validationErrorHandler, createUser)
+//! Routes without authentication
+// POST /api/users/create-user
+router.post("/v1/users/create-user", signUpUserValidationRules, validationErrorHandler, createUser)
 // POST /api/users/login
 router.post("/v1/users/login", loginUserValidationRules, validationErrorHandler, loginUser)
 // POST /api/users/refreshToken
-router.post("/v1/users/refreshToken", refreshTokenValidationRule, validationErrorHandler, refreshToken)
+router.post("/v1/users/refresh-token", refreshTokenValidationRule, validationErrorHandler, refreshToken)
 // POST/api/users/logout
 router.post("/v1/users/logout", refreshTokenValidationRule, validationErrorHandler, logoutUser)
+//! Routes with authentication
+// POST /api/users/signup
+router.post("/v1/users/admin/create-user", signUpUserValidationRules, validationErrorHandler, auth, adminCreateUser)
 
 export default router
