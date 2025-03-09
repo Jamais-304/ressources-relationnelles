f61404492554d29f865b54a6de0d3b5ee09ba61a
maelbrize <94190489+maelbrize@users.noreply.github.com>
2025-03-09 15:06:53 +0100

    feat(M:Resource): add middleware for validatedAndPublishedAt and create indexes

--- backend/models/Resource.ts
+++ backend/models/Resource.ts
@@ -4,12 +4,27 @@ const resourceSchema = new mongoose.Schema({
 	authorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 	title: { type: String, required: true, trim: true },
 	contentGridfsId: { type: String, required: true },
-	category: { type: String, enum:['TEXT','HTML','VIDEO','AUDIO','IMAGE'], required: true, trim: true },
+	category: { type: String, enum: ['TEXT', 'HTML', 'VIDEO', 'AUDIO', 'IMAGE'], required: true, trim: true },
 	relationType: { type: String, required: true, trim: true },
 	status: { type: String, enum: ['DRAFT', 'PENDING', 'PUBLISHED'], default: 'DRAFT' },
 	validatedAndPublishedAt: { type: Date },
 	validatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 
 }, { timestamps: true }); // createdAt, updatedAt
+
+// Middleware/hooks
+resourceSchema.pre('save', function (next) {
+	// If the "status" field changes to "PUBLISHED" but the "validatedAndPublishedAt" field was empty:
+	if (this.isModified('status') && this.status === 'PUBLISHED' && !this.validatedAndPublishedAt) {
+		// automatically set the current date for "validatedAndPublishedAt" field
+		this.validatedAndPublishedAt = new Date();
+	}
+
+	next();
+});
+
+// Indexes
+resourceSchema.index({ authorId: 1 });
+resourceSchema.index({ title: 'text' });
 
 export default mongoose.model('Resource', resourceSchema);
