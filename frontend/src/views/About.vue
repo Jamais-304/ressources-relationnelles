4401eaa3c011aeb8b372640d144903751404bbfb
Guillaume Cugnet <guillaume@cugnet.eu>
2025-03-16 11:58:06 +0100

    feat(F:router): add basic examples of RouterLink usage
    Refs: #28

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -3,4 +3,8 @@
 
 <template>
   Explications sur l’application.
+
+  <div>
+    <RouterLink to="/">Retour à l’accueil</RouterLink>
+  </div>
 </template>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,4 +3,8 @@
 
 <template>
   Bienvenue sur (RE)SOURCES RELATIONNELLES !
+
+  <div>
+    <RouterLink to="/about">À propos</RouterLink>
+  </div>
 </template>
