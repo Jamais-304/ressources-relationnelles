2f6bdd43791a1b4c556cc2f06b2e29ba4b0a721a
Guillaume Cugnet <guillaume@cugnet.eu>
2025-03-18 16:35:27 +0100

    feat(F:App): implement Pinia
    Refs: #36

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,13 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import router from './routes/router'
 import App from './App.vue'
+import router from './routes/router'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.use(router)
+app.mount('#app')
