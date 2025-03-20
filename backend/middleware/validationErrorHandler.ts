e4104d710a5d52d62a4a2fa99050a607f36b9f77
Quentin <quentindumon352a@gmail.com>
2025-03-20 09:42:50 +0100

    refactor(C:Users): update controllers and middlewares to patch type error (return and Promise<void>)
    Refs: #16

--- backend/middleware/validationErrorHandler.ts
+++ backend/middleware/validationErrorHandler.ts
@@ -1,11 +1,12 @@
 import type { Request, Response, NextFunction } from "express"
 import { validationResult } from "express-validator"
 
-export const validationErrorHandler = async (req: Request, res: Response, next: NextFunction) => {
+export const validationErrorHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const error = validationResult(req)
 
     if (!error.isEmpty()) {
-        return res.status(400).json({ error: error.array() })
+        res.status(400).json({ error: error.array() })
+        return
     }
     next()
 }
