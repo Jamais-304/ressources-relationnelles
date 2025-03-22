882511a3a75fe0a7a23d92a3806759e0e99cd746
Quentin <quentindumon352a@gmail.com>
2025-03-22 17:01:01 +0100

    test(S:Tests): update app.ts to prevent multiple connections when starting controller tests
    Refs: #16

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,11 +11,12 @@ const app = express()
 const swaggerDocs = yaml.load("swagger.yaml")
 
 // Conection to MongoDB Atlas DataBase
-mongoose
-    .connect(MONGO_URI)
-    .then(() => console.log("Connexion à MongoDB réussie ✅"))
-    .catch(() => console.log("Connexion à MongoDB échouée ❌"))
-
+if (process.env.NODE_ENV !== 'test') {
+    mongoose
+        .connect(MONGO_URI)
+        .then(() => console.log("Connexion à MongoDB réussie ✅"))
+        .catch(() => console.log("Connexion à MongoDB échouée ❌"))
+}
 app.use((req: Request, res: Response, next: NextFunction) => {
     res.setHeader("Access-Control-Allow-Origin", "*") // Allows access to the API from any origin
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, userid") // Allows the specified headers in requests to the API
