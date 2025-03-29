7aa2e929d4fdc7fd34da5c650bf270c1da469773
Guillaume Cugnet <guillaume@cugnet.eu>
2025-03-29 18:46:08 +0100

    feat(F:App): import vuetify
    Refs: #36

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,11 +6,21 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import Vue3Toastify from 'vue3-toastify'
 import 'vue3-toastify/dist/index.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const pinia = createPinia()
 const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
-app.use(pinia)
-app.use(router)
-app.use(Vue3Toastify, { autoClose: 2500, clearOnUrlChange: false })
-app.mount('#app')
+app
+  .use(pinia)
+  .use(router)
+  .use(Vue3Toastify, { autoClose: 2500, clearOnUrlChange: false })
+  .use(vuetify)
+  .mount('#app')
