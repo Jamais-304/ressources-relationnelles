5d564c9400c59e8253cedbec73eee8500cccd94d
Guillaume Cugnet <guillaume@cugnet.eu>
2025-03-29 20:13:00 +0100

    feat(V:Login): use vuetify component with validation rules
    Refs: #36

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -6,22 +6,30 @@ import { toast } from 'vue3-toastify'
 
 const api = new Api()
 
+const validLoginForm = ref(false)
+
 const email = ref('admintest@test.com')
 const password = ref('admintest')
 
+const emailRules = [
+  (v: string) => !!v || 'Ce champ ne peut pas être vide.',
+  (v: string) => /.+@.+\..+/.test(v) || "L'adresse email n'est pas valide.",
+]
+
 async function login() {
-  const attrs = {
-    email: email.value,
-    password: password.value
-  }
-  try {
-    const response = await api.users.login(attrs)
-    response.access && toast.success('Bienvenue !') && router.push('/')
-  } catch (error) {
-    toast.error(error)
+  if (validLoginForm.value) {
+    const attrs = {
+      email: email.value,
+      password: password.value,
+    }
+    try {
+      const response = await api.users.login(attrs)
+      response.access && toast.success('Bienvenue !') && router.push('/')
+    } catch (error) {
+      toast.error(error)
+    }
   }
 }
-
 </script>
 
 <template>
@@ -29,13 +37,26 @@ async function login() {
     <RouterLink to="/">Retour à l’accueil</RouterLink>
   </div>
 
-  <div>Email</div>
-  <input v-model="email" placeholder="test@test.com"></input>
+  <v-form v-model="validLoginForm" lazy-validation @submit.prevent="login">
+    <v-container>
+      <div class="flex flex-col w-[20vw]">
+        <v-text-field
+          v-model="email"
+          label="Email"
+          :rules="emailRules"
+        ></v-text-field>
 
-  <div>Mot de passe</div>
-  <input v-model="password" placeholder=""></input>
+        <v-text-field
+          v-model="password"
+          :counter="10"
+          label="Mot de passe"
+          :rules="[(v) => !!v || 'Ce champ ne peut pas être vide.']"
+        ></v-text-field>
+      </div>
+    </v-container>
 
-  <div>
-    <button @click="login()">Se connecter</button>
-  </div>
+    <v-card-actions>
+      <v-btn type="submit">Se connecter</v-btn>
+    </v-card-actions>
+  </v-form>
 </template>
