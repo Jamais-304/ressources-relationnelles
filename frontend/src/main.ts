8616195b947acefde53b0164e80fb910a9682121
Guillaume Cugnet <guillaume@cugnet.eu>
2025-03-29 16:39:21 +0100

    feat(F:App): parameter Vue3Toastify
    Refs: #36

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,10 +4,13 @@ import App from './App.vue'
 import router from './routes/router'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Vue3Toastify from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
 app.use(router)
+app.use(Vue3Toastify, { autoClose: 2500, clearOnUrlChange: false })
 app.mount('#app')
