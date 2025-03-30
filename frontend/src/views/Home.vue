51c37f652908a6e4df2344231ee94bea157da472
Guillaume Cugnet <guillaume@cugnet.eu>
2025-03-30 13:35:18 +0200

    feat(V:Home): enhance page with user information to debug
    Refs: #36

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
+import { useAuthUserStore } from '@/stores/authUserStore'
+import { storeToRefs } from 'pinia'
+
+const { username, roles } = storeToRefs(useAuthUserStore())
 </script>
 
 <template>
-  Bienvenue sur (RE)SOURCES RELATIONNELLES !
+  <v-container>
+    Bienvenue sur la page d’accueil !
 
-  <div>
-    <RouterLink to="/about">À propos</RouterLink>
-  </div>
-  <div>
-    <RouterLink to="/login">Page de connexion</RouterLink>
-  </div>
+    <v-card class="pa-4 ma-4" elevation="1" v-if="username">
+      <div>Nom: {{ username }}</div>
+      <div>
+        Roles:
+        <template v-for="(role, index) in roles" :key="index">
+          <span v-if="index < roles.length - 1"> {{ role }},</span>
+          <span v-else> {{ role }} </span>
+        </template>
+      </div>
+    </v-card>
+  </v-container>
 </template>
