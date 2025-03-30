7579d520f89f0f9a8f53a245738c09cef66c29a9
Quentin <quentindumon352a@gmail.com>
2025-03-30 17:28:21 +0200

    feat(S:Middlewares): enhance security for middleware userValidator
    Refs: #16

--- backend/src/middleware/validators/userValidator.ts
+++ backend/src/middleware/validators/userValidator.ts
@@ -27,6 +27,54 @@ export const signUpUserValidationRules = [
         .isLength({ min: 5, max: 40 })
         .withMessage("Pseudonym must be at least 5 characters and less than 40 characters")
         .escape()
+        .trim(), // Remove leading and trailing spaces
+    
+     // Role: string, required, equals escape special characters
+    body("role")
+        .isString()
+        .withMessage("Role must be a string")
+        .equals("utilisateur")
+        .withMessage("Invalid role")
+        .escape()
+        .trim() // Remove leading and trailing spaces
+]
+// Validation rules for user sign-up
+export const adminCreateUserValidationRules = [
+    // Email: must be a valid email, escape special characters
+    body("email")
+        .isEmail()
+        .withMessage("Email must be a valid email address")
+        .normalizeEmail() // Normalize the email
+        .escape()
+        .trim(), // Remove leading and trailing spaces
+
+    // Password: string, required, escape special characters
+    body("password")
+        .isString()
+        .withMessage("Password must be a string")
+        .isLength({ min: 8, max: 25 })
+        .withMessage("Password must be at least 8 characters and less than 26 characters")
+        .escape()
+        .trim(), // Remove leading and trailing spaces
+
+    // Pseudonym: string, required, escape special characters
+    body("pseudonyme")
+        .isString()
+        .withMessage("Pseudonym must be a string")
+        .isLength({ min: 5, max: 40 })
+        .withMessage("Pseudonym must be at least 5 characters and less than 40 characters")
+        .escape()
+        .trim(), // Remove leading and trailing spaces
+
+    // Role: string, required, escape special characters
+    body("role")
+        .isString()
+        .withMessage("Role must be a string")
+        .isLength({ min: 10, max: 20})
+        .withMessage("Role must be at least 10 characters and less than 20 characters")
+        .isIn(["utilisateur", "moderateur", "super-administrateur", "administrateur"])
+        .withMessage("Role must be one of the following: utilisateur, moderateur, super-administrateur, administrateur")
+        .escape()
         .trim() // Remove leading and trailing spaces
 ]
 
