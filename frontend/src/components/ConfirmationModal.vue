50691f1bb78e4fe160a63be011e03576c380290e
Guillaume Cugnet <guillaume@cugnet.eu>
2025-03-31 22:43:01 +0200

    feat(V:Users): allow deleting a user with confirmation modal
    Refs: #54

---
+++ frontend/src/components/ConfirmationModal.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const props = defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+  item: {
+    required: false,
+  },
+})
+
+const isVisible = defineModel({ type: Boolean, required: true })
+
+const emit = defineEmits(['confirm'])
+
+function close() {
+  isVisible.value = false
+}
+
+function confirm() {
+  emit('confirm', props.item)
+  close()
+}
+</script>
+
+<template>
+  <v-dialog v-model="isVisible" max-width="400px">
+    <v-card>
+      <v-card-title class="headline"> Confirmation </v-card-title>
+      <v-card-text>
+        <div>
+          {{ message }}
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn @click="close"> Annuler </v-btn>
+        <v-btn @click="confirm"> Confirmer </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
--- frontend/src/views/users/Users.vue
+++ frontend/src/views/users/Users.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import { Api, User } from '@/api/api'
+import ConfirmationModal from '@/components/ConfirmationModal.vue'
 import { onMounted, ref } from 'vue'
+import { toast } from 'vue3-toastify'
 
 const api = new Api()
 
 const users = ref<User[]>()
+const currentUser = ref<User>()
+
+const confirmationModalVisible = ref(false)
+const confirmationModalMessage = ref('')
 
 const headers = [
   {
@@ -32,6 +38,11 @@ const headers = [
     value: 'updatedAt',
     sortable: true,
   },
+  {
+    title: '',
+    key: 'actions',
+    sortable: false,
+  },
 ]
 
 async function listUsers() {
@@ -43,11 +54,45 @@ async function listUsers() {
   }
 }
 
+function askForDeleteConfirmation(user: User) {
+  currentUser.value = user
+  confirmationModalVisible.value = true
+  confirmationModalMessage.value = `Voulez-vous vraiment supprimer l’utilisateur ${user.username} ?`
+}
+
+async function deleteUser(user: User) {
+  try {
+    const response = await api.users.delete(user)
+    console.log(response)
+    if (response.message) {
+      toast.success(response.message)
+    }
+    await listUsers()
+  } catch (error) {
+    console.error(`Error while listing users: ${error}.`)
+  }
+}
+
 onMounted(() => listUsers())
 </script>
 
 <template>
   <v-card class="pa-4 ma-4" elevation="1">
-    <v-data-table :items="users" :headers="headers"> </v-data-table>
+    <v-data-table :items="users" :headers="headers">
+      <template #item.actions="{ item }">
+        <span class="flex gap-2">
+          <v-btn color="#f1c6be" @click="() => askForDeleteConfirmation(item)">
+            Supprimer
+          </v-btn>
+        </span>
+      </template>
+    </v-data-table>
   </v-card>
+
+  <ConfirmationModal
+    v-model="confirmationModalVisible"
+    :message="confirmationModalMessage"
+    :item="currentUser"
+    @confirm="deleteUser"
+  ></ConfirmationModal>
 </template>
