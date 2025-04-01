11580845c0e3b285227b0b9e3300d4fed4208410
Guillaume Cugnet <guillaume@cugnet.eu>
2025-04-01 16:04:58 +0200

    refactor(F:App): store a full User object in authUserStore
    Refs: #54

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,16 +2,16 @@
 import { useAuthUserStore } from '@/stores/authUserStore'
 import { storeToRefs } from 'pinia'
 
-const { username, role } = storeToRefs(useAuthUserStore())
+const { authUser } = storeToRefs(useAuthUserStore())
 </script>
 
 <template>
   <v-container>
     Bienvenue sur la page d’accueil !
 
-    <v-card class="pa-4 ma-4" elevation="1" v-if="username">
-      <div>Nom: {{ username }}</div>
-      <div>Role: {{ role }}</div>
+    <v-card class="pa-4 ma-4" elevation="1" v-if="authUser">
+      <div>Nom: {{ authUser?.username }}</div>
+      <div>Role: {{ authUser?.role }}</div>
     </v-card>
   </v-container>
 </template>
