c0ea224c0d323d9310de45fb868156f87c086e46
Quentin <quentindumon352a@gmail.com>
2025-04-03 11:30:35 +0200

    docs(S:Back): add minor modifications and update documentation
    Refs: #65

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,22 +1,27 @@
 import express from "express"
 import type { Request, Response, NextFunction } from "express"
 import mongoose from "mongoose"
-import { MONGO_URI } from "../config.ts"
+import { MONGO_URI } from "./config.ts"
 import yaml from "yamljs"
 import swaggerUi from "swagger-ui-express"
 import userRouter from "./router/userRoutes.ts"
 import refreshTokenRouter from "./router/refreshTokenRoute.ts"
+import { morganMiddleware, errorLogger } from "../logs/logger.ts"
 
 const app = express()
+app.set("trust proxy", true)
+
+// Swagger documentation
 const swaggerDocs = yaml.load("swagger.yaml")
 
-// Conection to MongoDB Atlas DataBase
-if (process.env.NODE_ENV !== 'test') {
+// Connection to MongoDB Atlas Database
+if (process.env.NODE_ENV !== "test") {
     mongoose
         .connect(MONGO_URI)
         .then(() => console.log("Connexion à MongoDB réussie ✅"))
         .catch(() => console.log("Connexion à MongoDB échouée ❌"))
 }
+// CORS middleware to allow cross-origin requests
 app.use((req: Request, res: Response, next: NextFunction) => {
     res.setHeader("Access-Control-Allow-Origin", "*") // Allows access to the API from any origin
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, userid") // Allows the specified headers in requests to the API
@@ -24,10 +29,14 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     next()
 })
 
+// JSON parsing middleware
 app.use(express.json())
+app.use(morganMiddleware) // For request logging
 // Routes Users
 app.use("/api", userRouter, refreshTokenRouter)
-// Docuementation API
+// Swagger API documentation route
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+// Error handling middleware should be after routes to catch errors
+app.use(errorLogger) // Logs any unhandled errors
 
 export default app
